b1878d198e3e4d2f070c0ceb37102c155cfefded
Aleksey Kabkov <aleksey.kabkov@gmail.com>
2025-07-12 05:03:55 +0300

    Add JupyterLab configuration

--- Neovim/lua/plugins/leap.lua
+++ Neovim/lua/plugins/leap.lua
@@ -1,17 +1,17 @@
 -- Leap.nvim — быстрые прыжки по тексту
 
 return {
-  'ggandor/leap.nvim',
+  url = "https://codeberg.org/andyg/leap.nvim",
   config = function()
     local leap = require('leap')
 
     -- Прыжок в текущем окне
-    vim.keymap.set({ 'n', 'v', 'x', 's', 'o' }, '<leader>s', function()
+    vim.keymap.set({ 'n', 'v', 'x', 's', 'o' }, '<leader>f', function()
       leap.leap({ target_windows = { vim.fn.win_getid() } })
     end, { desc = 'Leap: обычный прыжок' })
 
     -- Прыжок во всех окнах
-    vim.keymap.set({ 'n', 'v', 'x', 's', 'o' }, '<leader>S', function()
+    vim.keymap.set({ 'n', 'v', 'x', 's', 'o' }, '<leader>F', function()
       leap.leap({ target_windows = require('leap.util').get_enterable_windows() })
     end, { desc = 'Leap: во всех окнах' })
   end,
